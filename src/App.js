5332f7d195951ab4f9e2e8e3c655273cf07f29f7
Leesin0222 <dragon02229@naver.com>
2022-10-02 23:03:55 +0900

    ⚡ :: Context api

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TodoTemplate from "./component/TodoTemplate";
 import TodoHead from "./component/TodoHead";
 import TodoList from "./component/TodoList";
 import TodoCreate from "./component/TodoCreate";
+import { TodoProvider } from "./TodoContext";
 
 const GlobalStyle = createGlobalStyle`
 body {
@@ -13,14 +14,14 @@ body {
 
 function App() {
   return (
-    <>
+    <TodoProvider>
       <GlobalStyle />
       <TodoTemplate>
         <TodoHead/>
           <TodoList/>
           <TodoCreate/>
       </TodoTemplate>
-    </>
+    </TodoProvider>
   );
 }
 
