1ef175df7c115ae437ff35f24e8d736acd7f6184
Leesin0222 <dragon02229@naver.com>
2022-10-02 16:49:01 +0900

    ⚙️⚡ :: React 세팅, 컴포넌트 맹들어줌

---
+++ src/TodoContext.js
@@ -0,0 +1,48 @@
+import React, {useReducer} from "react";
+
+const initialTodos = [
+    {
+        id: 1,
+        text: "JavaScript 공부하기",
+        done: true
+    },
+    {
+        id: 2,
+        text: "TypeScript 공부하기",
+        done: true
+    },
+    {
+        id: 3,
+        text: "Recoil 공부하기",
+        done: true
+    },
+    {
+        id: 4,
+        text: "리액트 공부하기",
+        done: false
+    },
+    {
+        id: 5,
+        text: "css 공부하기",
+        done: false
+    }
+];
+
+function todoReducer(state, action) {
+    switch (action.type) {
+        case 'CREATE':
+            return state.concat(action.todo);
+        case 'TOGGLE':
+            return state.map(todo =>
+                todo.id === action.id ? {...todo, done: !todo.done} : todo);
+        case 'REMOVE':
+            return state.filter(todo => todo.id !== action.id);
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
+export function TodoProvider({children}) {
+    const [state, dispatch] = useReducer(todoReducer, initialTodos);
+    return children;
+}
